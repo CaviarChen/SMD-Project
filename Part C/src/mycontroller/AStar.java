2c3477d8cb21bd7366cf72e748db046cfb6991be
Eana Hufwe <ilove@1a23.com>
2018-05-24 22:58:20 +1000

    Initial Recovery stratergy

--- Part C/src/mycontroller/AStar.java
+++ Part C/src/mycontroller/AStar.java
@@ -2,32 +2,70 @@ package mycontroller;
 
 import tiles.LavaTrap;
 import utilities.Coordinate;
+
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 
 public class AStar implements Pipeline.Step<ArrayList<Position>, MapRecorder> {
 
+    class Node implements Comparable<Node> {
+        public Coordinate coord; // coordinate
+        public Node parent; // parent
+        public int G; // G: correct value, from start to current node
+        public int H; // H: estimated value, from current node to end
+
+        public Node(int x, int y) {
+            this.coord = new Coordinate(x, y);
+        }
+
+        public Node(Coordinate coord, Node parent, int g, int h) {
+            this.coord = coord;
+            this.parent = parent;
+            G = g;
+            H = h;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            if (o == null) return -1;
+            if (G + H > o.G + o.H)
+                return 1;
+            else if (G + H < o.G + o.H) return -1;
+            return 0;
+        }
+    }
+
     @Override
     public ArrayList<Position> execute(ArrayList<Position> input, MapRecorder mapRecorder) {
+
+        Position destination = input.get(input.size() - 1);
+        input.remove(input.size() - 1);
+
+        Node end = start(mapRecorder, destination, input);
+
+        // path
+        ArrayList<Position> path = new ArrayList<>();
+
+        while (end != null) {
+            path.add(new Position(end.coord));
+            end = end.parent;
+        }
+        return path;
+
+    }
+
+    public Node start(MapRecorder mapRecorder, Position source, ArrayList<Position> destination) {
 
         PriorityQueue<Node> openList = new PriorityQueue<>();
         ArrayList<Node> closeList = new ArrayList<>();
 
-        openList.clear();
-        closeList.clear();
+        Node end = new Node(Math.round(source.x), Math.round(source.y));
 
 
-
-        for (int i=0; i<input.size()-1; i++) {
-            openList.add(new Node(Math.round(input.get(i).x), Math.round(input.get(i).y)));
+        for (Position i: destination) {
+            openList.add(new Node(Math.round(i.x), Math.round(i.y)));
         }
 
-        Node end = new Node(Math.round(input.get(input.size()-1).x), Math.round(input.get(input.size()-1).y));
-
-        return start(mapRecorder, openList, closeList, end);
-    }
-
-    public ArrayList<Position> start(MapRecorder mapRecorder, PriorityQueue<Node> openList, ArrayList<Node> closeList, Node end) {
         // start search
         while (!openList.isEmpty()) {
 //            if (isCoordInClose(closeList, end.coord)) break;
@@ -37,14 +75,8 @@ public class AStar implements Pipeline.Step<ArrayList<Position>, MapRecorder> {
             closeList.add(current);
             if (current != null) addNeighborNodeInOpen(mapRecorder, openList, closeList, end, current);
         }
-        // path
-        ArrayList<Position> path = new ArrayList<>();
 
-        while (end != null) {
-            path.add(new Position(end.coord));
-            end = end.parent;
-        }
-        return path;
+        return end;
     }
 
     // Manhattan distance as heuristic distance
@@ -140,31 +172,4 @@ public class AStar implements Pipeline.Step<ArrayList<Position>, MapRecorder> {
 
 }
 
-class Node implements Comparable<Node> {
 
-    public Coordinate coord; // coordinate
-    public Node parent; // parent
-    public int G; // G: correct value, from start to current node
-    public int H; // H: estimated value, from current node to end
-
-    public Node(int x, int y) {
-        this.coord = new Coordinate(x, y);
-    }
-
-    public Node(Coordinate coord, Node parent, int g, int h) {
-        this.coord = coord;
-        this.parent = parent;
-        G = g;
-        H = h;
-    }
-
-    @Override
-    public int compareTo(Node o) {
-        if (o == null) return -1;
-        if (G + H > o.G + o.H)
-            return 1;
-        else if (G + H < o.G + o.H) return -1;
-        return 0;
-    }
-
-}
--- Part C/src/mycontroller/RepairStrategy.java
+++ Part C/src/mycontroller/RepairStrategy.java
@@ -6,22 +6,25 @@ import utilities.Coordinate;
 import java.util.ArrayList;
 
 public class RepairStrategy implements Strategy {
+
+    private static final int FINISH_THRESHOLD = 96;
+    private static final double DISTANCE_FACTOR = 0.5;
+    private static final double TAKE_OVER_THRESHOLD = 50;
+
     @Override
     public ArrayList<Position> getTargets(MyAIController myAIController) {
 
         int currentX = Math.round(myAIController.getX());
         int currentY = Math.round(myAIController.getY());
 
-
         // already on HealthTrap
         if (myAIController.mapRecorder.mapTiles[currentX][currentY] instanceof HealthTrap) {
             return null;
         }
 
-
         ArrayList<Position> output = new ArrayList<>();
 
-        for(Coordinate coord: myAIController.mapRecorder.healthCoords) {
+        for (Coordinate coord : myAIController.mapRecorder.healthCoords) {
             output.add(new Position(coord));
         }
 
@@ -30,11 +33,29 @@ public class RepairStrategy implements Strategy {
 
     @Override
     public boolean isFinished(MyAIController myAIController) {
-        return (myAIController.getHealth() >= 100);
+        return (myAIController.getHealth() >= FINISH_THRESHOLD);
     }
 
     public boolean needTakeover(MyAIController myAIController) {
+        // Never take over if just got out from the health trap
 
-        return ((myAIController.getHealth() < 50) && myAIController.mapRecorder.healthCoords.size()>0);
+        // Never take over if no health trap is found
+        if (myAIController.mapRecorder.healthCoords.isEmpty()) return false;
+
+        ArrayList<Position> destination = new ArrayList<>();
+        for (Coordinate i: myAIController.mapRecorder.healthCoords) {
+            destination.add(new Position(i));
+        }
+
+
+        AStar.Node node = new AStar().start(myAIController.mapRecorder,
+                new Position(myAIController.getX(), myAIController.getY()),
+                destination
+        );
+
+        System.out.println("TAKE OVER EVALUATION VALUE = " + (myAIController.getHealth() + node.G * DISTANCE_FACTOR));
+
+        return myAIController.getHealth() + node.G * DISTANCE_FACTOR <= TAKE_OVER_THRESHOLD;
     }
+
 }
