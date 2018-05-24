9cb83cbef2d10011282ba23069c52ea2eefad8ca
CaviarChen <yzj@vlabpro.com>
2018-05-24 14:23:32 +1000

    update Pipeline

--- Part C/src/mycontroller/Pipeline.java
+++ Part C/src/mycontroller/Pipeline.java
@@ -2,28 +2,28 @@ package mycontroller;
 
 import java.util.ArrayList;
 
-public class Pipeline<T> {
+public class Pipeline<T, D> {
 
-    ArrayList<Step<T>> steps;
+    ArrayList<Step<T, D>> steps;
 
     public Pipeline() {
         steps = new ArrayList<>();
     }
 
-    public void appendStep(Step<T> step) {
+    public void appendStep(Step<T, D> step) {
         steps.add(step);
     }
 
-    public T execute(T input) {
-        for(Step<T> step: steps) {
-            input = step.execute(input);
+    public T execute(T input, D data) {
+        for(Step<T, D> step: steps) {
+            input = step.execute(input, data);
         }
 
         return input;
     }
 
 
-    public interface Step<T> {
-        T execute(T input);
+    public interface Step<T, D> {
+        T execute(T input, D data);
     }
 }
